53d2fe92d1492760dfd2e88bc10d2525e7596324
mongy <=>
2023-07-01 02:30:04 +0300

    delete command

--- scripts/api/commands/interfaces/executable_builder.py
+++ scripts/api/commands/interfaces/executable_builder.py
@@ -1,6 +1,7 @@
 import requests
 from scripts.models import BaseCommand
 BuilderTest = 'http://localhost:8001/build/python/'
+DeleteURL = 'http://localhost:8001/executable'
 
 
 def build_script(command_id, command_name, files):
@@ -12,4 +13,9 @@ def build_script(command_id, command_name, files):
                                  'script': script.file or files['script'],
                                  'requirements': script.dependency or files['requirements']
                              })
+    return response
+
+
+def remove_executable(old_executable_link):
+    response = requests.delete(DeleteURL, params={'old_executable_link': old_executable_link})
     return response
