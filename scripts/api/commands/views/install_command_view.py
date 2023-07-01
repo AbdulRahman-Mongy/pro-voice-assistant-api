ea283b19777200f1f62a2d2c23eed10c0b8c48be
mongy <=>
2023-07-01 19:53:25 +0300

    fix the is_installed? logic

--- scripts/api/commands/views/install_command_view.py
+++ scripts/api/commands/views/install_command_view.py
@@ -19,7 +19,7 @@ class InstallCommand(generics.RetrieveAPIView):
     def get(self, request, *args, **kwargs):
         command = self.get_object()
         user = self.request.user
-        already_installed = BaseCommand.objects.filter(used_by__id=user.id) or False
+        already_installed = BaseCommand.objects.filter(pk=command.id).filter(used_by__id=user.id) or False
         if not already_installed:
             command.used_by.add(user)
         return self.retrieve(request, *args, **kwargs)
@@ -38,7 +38,7 @@ class UninstallCommand(generics.DestroyAPIView):
     def delete(self, request, *args, **kwargs):
         command = self.get_object()
         user = self.request.user
-        already_installed = BaseCommand.objects.filter(used_by__id=user.id) or False
+        already_installed = BaseCommand.objects.filter(pk=command.id).filter(used_by__id=user.id) or False
         if already_installed:
             command.used_by.remove(user)
             return Response(status=status.HTTP_204_NO_CONTENT)
